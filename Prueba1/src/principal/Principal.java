87659026ee5be89b940efdb652987c580409876c
Victor <victorgonzalez862@gmail.com>
2021-04-16 12:23:10 +0200

    Nueva subida en cambio 1

--- Prueba1/src/principal/Principal.java
+++ Prueba1/src/principal/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo Proyecto Git-Java");
 		System.out.println("Hola de nuevo");
+		System.out.println("Cambio en cambio1");
 	}
 
 }
