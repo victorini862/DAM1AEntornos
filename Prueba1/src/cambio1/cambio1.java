ebe557c5ce43beecd6a28828c12fa0e5fcf71603
Victor <victorgonzalez862@gmail.com>
2021-04-21 13:46:00 +0200

    Validacion de la clase Cambio1 cambio1

---
+++ Prueba1/src/cambio1/cambio1.java
@@ -0,0 +1,11 @@
+package cambio1;
+
+public class cambio1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola en cambio1");
+		System.out.println("Hola de nuevo en cambio1");
+
+	}
+
+}
